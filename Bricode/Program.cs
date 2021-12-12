82a0af09b351884b81c233465f0e21262a7aa8a9
julienGith <53513531+julienGith@users.noreply.github.com>
2021-12-12 13:12:01 +0100

    up

---
+++ Bricode/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+namespace Bricode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+            int[] tableau = Enumerable.Range(1, 50).ToArray();
+            rechercherValeur.RechercheDansTableauOrdonne(12, tableau);
+            System.Console.ReadLine();
+            static void Menu()
+            {
+                Console.WriteLine("Hello World!");
+            }
+        }
+
+    }
+}
