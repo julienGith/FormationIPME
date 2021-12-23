d1f1a69c7e1f42d3199b37722c630c358f12350d
julienGith <53513531+julienGith@users.noreply.github.com>
2021-12-23 17:52:43 +0100

    Create Trotinette.cs

---
+++ Garage/Garage/Trotinette.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage
+{
+    internal class Trotinette : Vehicle
+    {
+        public Trotinette(int id, string name, Const.EtatVehicle state, string model, string brand, uint kilometrage, uint type, int puissance) : base(id, name, state, model, brand, kilometrage, type)
+        {
+        }
+    }
+}
